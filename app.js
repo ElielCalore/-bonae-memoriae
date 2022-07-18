f1958ebd80fa1b4b0a643f540266b4ee0b700146
eliel <elieldscalore@gmail.com>
2022-07-18 20:36:11 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+require("./config/db.config")();
+const app = express();
+
+app.use(express.json());
+
+const userRouter = require("./routes/users.routes");
+app.use("/user", userRouter);
+
+const memoryRouter = require("./routes/memories.routes");
+app.use("/memory", memoryRouter);
+
+const albunsRouter = require("./routes/albuns.routes");
+app.use("/album", albunsRouter);
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`Servidor ligado a porta: ${process.env.PORT}`);
+});
